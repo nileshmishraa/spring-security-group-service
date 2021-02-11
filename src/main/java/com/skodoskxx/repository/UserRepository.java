0b852f459f579f679d3edbedc6fdea31ad3d0220
Nilesh Mishra <cnilesh97@gmail.com>
2021-02-11 12:47:14 +0530

    Initial commit

---
+++ src/main/java/com/skodoskxx/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.skodoskxx.repository;
+
+import com.skodoskxx.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUserName(String username);
+}
